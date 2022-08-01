fc1f770e1069b7a2be7c635810db2f782d78cbb1
DANIELGEDAMO <danielgadamo@gmail.com>
2022-08-01 08:50:20 +0300

    g

--- festivals/fest.js
+++ festivals/fest.js
@@ -9,15 +9,35 @@ function showData() {
     let date = item.date;
     let location = item.location;
     listContainer.innerHTML += 
-    `<div class= "card ">
+    `<div class= "card col-md-3 ">
     <h2>${title}</h2>
-    <img  src="${image} "<button  type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal">
-    <p>${description}</p>
-  </button>"
+    <img class="img-fluid img-top" src="${image} ">
+    <button class="btn btn-outline-primary fstivalbtn ">O</button>
+    <p class = "d-none dscrip"><b>Description:</b>${description}</p>
+    <P><b>Date:</b>${date}</p>
+    <p><b>Location:</b>${location}</p>
     <a href="${embedUrls}"><button type="button" class="btn btn-outline-primary">see more</button></a>
     </div>
    `;
   });
+  showDescription()
 }
 showData();
+
+
+
+ 
+function showDescription(){
+
+document.querySelectorAll(".fstivalbtn").forEach((item,i)=>{
+    // console.log(item);
+    item.addEventListener("click",()=>{
+  
+    document.querySelectorAll(".dscrip")[i].classList.toggle("d-block")
+  document.querySelectorAll(".dscrip")[i].classList.toggle("d-none");
+  
+    })
+  })
+}
+
 

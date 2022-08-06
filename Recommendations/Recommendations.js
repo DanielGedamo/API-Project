87292dd21a661f8c6dff8906a7de614828f1a0c4
DANIELGEDAMO <danielgadamo@gmail.com>
2022-08-07 00:57:27 +0300

    w

--- Recommendations/Recommendations.js
+++ Recommendations/Recommendations.js
@@ -3,7 +3,7 @@ let usersDIV = document.getElementById("container");
 let loading = document.getElementById("loading");
 async function getUsers(){
   try{    
-   loading.innerText = "Loading..."
+   loading.innerHTML = "<img src=/imags/Loading_icon.gif>"
 
       let users = await fetch(baseApi).then(
         (res) => res.json())
@@ -15,7 +15,7 @@ async function getUsers(){
     alert(err)
   } 
   finally {
-    loading.innerText=""
+    loading.innerHTML=""
   }
  }
  let counter=1;
@@ -24,11 +24,10 @@ async function getUsers(){
    
    <div class ="card">
    <div ><img src="https://randomuser.me/api/portraits/men/${counter++}.jpg"></div>
-   <p> name:${user.name.first} ${user.name.last}</p>
-   <p>age:${user.age}</p>
-   <p>phone number:${user.phone}</p>
-   <p>email:${user.email}</p>
-   <p>:${user.email}</p>
+   <p> Name:${user.name.first} ${user.name.last}</p>
+   <p>Age:${user.age}</p>
+   <p>Phone number:${user.phone}</p>
+   <p>${user.email}</p>
    </div>`;
   }
  
@@ -38,30 +37,3 @@ async function getUsers(){
 
 
 
-// let userUrl = "https://my-json-server.typicode.com/Jeck99/fake-server/users";
-// let userTable = document.getElementById("userTable");
-// let loading = document.getElementById("loading");
-// async function getUsers() {
-//   loading.innerText = "Loading..."
-
-//   let users = await fetch(userUrl).then(
-//     (res) => res.json(),
-//     (e) => alert(e)
-//   );
-//   if (users && users.length) {
-//     users.forEach((user) => {
-//       userTable.innerHTML += createTable(user);
-//     });
-//   }
-//   loading.innerText=""
-// }
-// function createTable(user) {
-//   return `
-// <tr><td>${user.email}</td> 
-// <td>${user.phone}</td> 
-// <td>${user.age}</td>
-// <td>${user.name.last}</td>
-// <td>${user.name.first}</td>
-// <td><img src= /imags/Boom-2018.jpg></td></tr>
-// `;
-// }
